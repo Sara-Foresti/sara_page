6d48a3d5566d9636c402ade6a3263f418cc21f84
Sara Foresti <sforesti@houseofkaizen.com>
2020-08-05 16:11:39 +0100

    working on jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,27 @@
+// import $ from 'jQuery';
+
 const navbar = document.querySelector('.sf-navbar');
 
 const bodyHeight = document.body.getBoundingClientRect().height;
 const mastheadHeight = document.querySelector('.sf-masthead').getBoundingClientRect().height;
+
 const totalToScroll =  bodyHeight - mastheadHeight;
 // const newZero = mastheadHeight - 115;
 
 window.addEventListener('scroll', function(){
 	if(navbar.getBoundingClientRect().top <= 0) {
 		navbar.classList.add('sf-fixed');
-		const percent = ((window.scrollY /*- newZero*/) * 100 ) / totalToScroll;
+		const percent = (window.scrollY * 100 ) / totalToScroll;
 		const alphaValue = percent / 100;
 		document.querySelector('.sf-fixed').style.backgroundColor =	'rgba(15, 8, 75,' + alphaValue + ')';
 	}
 	if (document.querySelector('.sf-masthead').getBoundingClientRect().bottom >= 115) {
 		navbar.classList.remove('sf-fixed');
+		navbar.style.backgroundColor =	'rgba(15, 8, 75, 0)';
 	}
 });
 
 
-
-
-
+$(".tab").on("click", function (e) {
+  console.log('blabla');
+});
